4c31be32ab022b9506f25bb6ce7b232ca74810a1
Mateusz <Mateusz@mateusz>
2014-05-28 11:04:46 +0200

    a

---
+++ src/projekt/Conductor.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package projekt;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+/**
+ *
+ * @author Mateusz
+ */
+public class Conductor implements State {
+    
+    @Override
+    public State nextState(int x, int y, TableBoard board) {
+        int counter = board.neighboursCounter(x, y);
+        if(counter == 1 || counter == 2) return ElectronHead;
+        else return Conductor;
+    }
+    
+}
